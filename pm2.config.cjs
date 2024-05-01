87b6a2daf9c02caac6b91f18c45f0fc62aa6d3da
yash91989201 <yashraj6370@gmail.com>
2024-05-01 12:19:21 +0530

    update pm2 config file to check auto deployment

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
 	name: "expense_tracker", // Name of your application
 	script: "./server/index.ts", // Entry point of your application
-	interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+	interpreter: "/home/yash/.bun/bin/bun", // Path to the Bun interpreter
 };
