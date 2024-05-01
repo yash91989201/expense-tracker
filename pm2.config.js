d560d6e1406ccd033ab03d65e3f0e26f1f233b8c
yash91989201 <yashraj6370@gmail.com>
2024-05-01 12:15:35 +0530

    add gh actions and pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	name: "expense_tracker", // Name of your application
+	script: "./server/index.ts", // Entry point of your application
+	interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+};
